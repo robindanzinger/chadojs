38b0fc7f0ce6737ea88f8d155e1602336973fe74
robindanzinger <robindanzinger@gmx.de>
2014-05-01 19:05:51 +0200

    config file

---
+++ test/chado.js
@@ -0,0 +1,28 @@
+var buster = require('buster');
+var assert = buster.assert;
+var refute = buster.refute;
+var chado = require('../lib/chado');
+buster.testCase("library chado", {
+  "has access to chado.testdoubleRepo" : function () {
+    assert(chado.testDoubleRepo);
+  },
+  "has access to chado.assume" : function () {
+    assert(chado.assume);
+  },
+  "has access to chado.verify" : function () {
+    assert(chado.verify);
+  },
+  "has access to chado.createDouble" : function () {
+    assert(chado.createDouble);
+  },
+  "if chado is loaded twice, same objects are used" : function () {
+    var chado2 = require('../lib/chado');
+    var assume = chado.assume;
+    var createDouble = chado.createDouble;
+    var lib = createDouble("myLib");
+    assume(lib).canHandle("foo").andReturn("bar");
+
+    assert(chado2.repo.myLib.foo);
+  }
+});
+
