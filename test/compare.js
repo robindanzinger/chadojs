f686911139d3ff0c5eefef7ae394cbb7c62e645d
robindanzinger <robindanzinger@gmx.de>
2015-01-23 19:28:33 +0100

    compare two objects

--- test/compare.js
+++ test/compare.js
@@ -11,9 +11,23 @@ buster.testCase("library compare", {
     refute(is('foo').similarTo('bar'));
     assert(is(true).similarTo(true));
     assert(is(false).similarTo(false));
+    refute(is(false).similarTo(''));
+    refute(is(4).similarTo('4'));
+    assert(is(new Date(2014, 1, 2)).similarTo(new Date(2014, 1, 2)));
+    assert(is(new Date(2013, 13, 1)).similarTo(new Date(2014, 1, 1)));
+    refute(is(new Date(2014, 1, 2)).similarTo(new Date(2015, 1, 2)));
   },
   "arrays should be similar, when all elements are equal and in same order" : function () {
     var array = ["first", "second", 3, true];
     assert(is(array).similarTo(["first", "second", 3, true]));
+    refute(is(array).similarTo(["first", "second", true, 3]));
+    refute(is(null).similarTo(array));
+    refute(is({}).similarTo(array));
+  },
+  "objects should be similar, when the second object contains the same properties as the first object" : function () {
+    var firstObject = {property : "value"};
+    assert(is(firstObject).similarTo({property:"value"}));
+    assert(is(firstObject).similarTo({property:"value",prop2:2}));
+    assert(is({prop:"val",dat:new Date(2014,0,1)}).similarTo({dat:new Date(2014,0,1),prop:"val"}));
   }
 })
