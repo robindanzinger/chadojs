feff66cbdcb09345cedd4c721c602883b83bfa27
robindanzinger <robindanzinger@gmx.de>
2015-01-24 18:59:29 +0100

    Rewritten repo_to_array_converter, more flexible and simpler

--- test/repo_to_array_converter.js
+++ test/repo_to_array_converter.js
@@ -1,95 +1,68 @@
-var buster = require('buster');
+var buster = require('buster'); 
 var assert = buster.assert;
 var refute = buster.refute;
-buster.testCase("Convert a map to an object" , {
-  "should contain all keys as property values" : function () {
-    var data = {
-    "Cache": {
-      "hasValue": {
-        "[\"anyUnknownId\"]": {
-          "false": {
-            "assume": {
-              "C:\\Users\\Robin\\Documents\\GitHub\\chadojs\\test\\example2.js": {
-                "16": {}
-              }
-            },
-            "calledBy": {
-              "C:\\Users\\Robin\\Documents\\GitHub\\chadojs\\test\\example2.js": {
-                "43": {
-                  "func": "Object.get"
-                }
-              }
-            }
-          }
-        }
-      }
+var fs = require('fs');
+buster.testCase("Chado Object Keys to Array Transformer", {
+  "It transforms the json map to an array" : {
+    "{} should result in []" : function () {
+      assert.equals([], transform({}));
+    },
+    "{A:{}} should result in [{1:A}]" : function () {
+      assert.equals([{1:'A'}], transform({A:{}}));
+    },
+    "{A:{},B:{}} should result in [{1:A},{1:B}]" : function () {
+      assert.equals([{1:'A'}, {1:'B'}], transform({A:{},B:{}}));
+    },
+    "{A:{B:{}}} should result in [{1:A,2:B}]" : function () {
+      assert.equals([{1:'A', 2:'B'}], transform({A:{B:{}}}));
+    },
+    "{A:{B:{C:{}}}} should result in [{1:A,2:B,3:C}]" : function () {
+      assert.equals([{1:'A', 2:'B', 3:'C'}], transform({A:{B:{C:{}}}}));
+    },
+    "{A:{C:{}}} should result in [{1:A,2:C}]" : function () {
+      assert.equals([{1:'A', 2:'C'}], transform({A:{C:{}}}));
+    },
+    "{A:{B:{}, C:{}}} should result in two objects A->B and A->C" : function () {
+      var array = transform({A:{B:{}, C:{}}});
+      assert.equals(array.length, 2);
+    },
+    "complex example" : function () {
+      this.json = {
+        ObjectA:{Method1:{'["Arg1"]':{ReturnVal1:{"assume":{"path1":{"14":{"CallerFuncA":{}}}}}}}},
+        ObjectB:{Method1:{'["Arg1"]':{ReturnVal1:{"assume":{"path2":{"14":{"CallerFuncB":{}}}},
+                                                  "verify":{"path2b":{"42":{"CallerFuncB2":{}}}}}}}},
+        ObjectC:{Method1:{'["Arg1"]':{ReturnVal1:{"assume":{"path3":{"14":{"CallerFuncC1":{}}}}}},
+                          '["Arg2"]':{ReturnVal1:{"assume":{"path4":{"17":{"CallerFuncC2":{}}}}}}}}
+      };
+      assert.equals(transform(this.json).length, 5); 
     }
-    };
-
-    var obj = {};
-    convertMapToObject(data, obj, "className", "methodName", "parameters", "returnValue");
-
-    assert.equals(obj.className, "Cache");
-    assert.equals(obj.returnValue, "false");
-    assert.equals(obj.parameters, "[\"anyUnknownId\"]");
-    assert.equals(obj.methodName, "hasValue"); 
-  },
-  "should convert map to array" : function () {
-    var map = { "Key1": {}, "Key2": {}};
-
-    var array = convertMapToArray(map);
-
-    assert.equals(array[0], {"Name" : "Key1", "Values" : []});
-    assert.equals(array[1], {"Name" : "Key2", "Values" : []});
-  },
-  "should convert map in map to array" : function () {
-    var map = { "Key1": {"SubKey11": {}, "SubKey12": {}}, "Key2": {"SubKey21":{}}};
-
-    var array = convertMapToArray(map);
-
-    assert.equals(array[0], {"Name" : "Key1", "Values" : [{"Name":"SubKey11","Values":[]},{"Name":"SubKey12","Values":[]}]});
-    assert.equals(array[1], {"Name" : "Key2", "Values" : [{"Name":"SubKey21","Values":[]}]});
-  },
-  "can limit convertion of submaps" : function () {
-    var map = { "Key1": {"prop":"value"}};
-
-    var array = convertMapToArray(map, 1);
-
-    assert.equals(array[0], {"Name" : "Key1", "Value" : {"prop":"value"}}); 
   }
 });
 
-function convertMapToArray(map, limit) {
-  if (limit === 1) {
-    return convertLastMapToArray(map);
-  }
-  return convertMapAndSubMapToArray(map, limit); 
+function transform(obj) {
+  return extractKeys(obj, []);
 }
 
-function convertLastMapToArray(map) {
-  var array = [];
-  for (prop in map) {
-    array.push({"Name":prop,"Value":map[prop]});
-  }
-  return array;
-} 
-
-function convertMapAndSubMapToArray(map, limit){
-  var array = [];
-  limit = limit > 1 ? limit -1 : undefined; 
-  for (prop in map) {
-    array.push({"Name":prop,"Values":convertMapToArray(map[prop], limit)});
-  }
-  return array; 
-} 
-
-function convertMapToObject(map, obj, propName) {
-  var restPropNames = Array.prototype.slice.call(arguments, 3);
-  for (var prop in map) {
-    obj[propName] = prop;
-    if (restPropNames.length > 0) {
-      var func = convertMapToObject.bind(null, map[prop], obj);
-      return func.apply(null, restPropNames);
+function extractKeys (obj, extractedKeys) {
+  var result = [];
+  if (Object.keys(obj).length > 0) {
+    for (var prop in obj) {
+      result = result.concat(extractKeys(obj[prop], extractedKeys.concat(prop))); 
     }
+  } else if (extractedKeys.length > 0){
+    result.push(createResult(extractedKeys));
   }
+  return result;
 }
+
+function createResult(props) {
+  var result = {};
+  props.forEach(function(key, index){
+    result[index+1] = key;
+  });
+  return result;
+}
+
+
+
+
