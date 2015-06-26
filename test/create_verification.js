3dc816d17c62a6ad871d0651b9f21f64a83e5183
robindanzinger <robindanzinger@gmx.de>
2015-06-26 23:40:08 +0200

    corrected createVerification String and create new test

--- test/create_verification.js
+++ test/create_verification.js
@@ -4,19 +4,31 @@ var expect = require('must');
 var stringify = require('../lib/stringify').stringify;
 var createVerification = require('../lib/create_verification');
 describe('Lib create_verification', function () {
-  it('create verify string for returnValue assumption', function () {
-    var expected = 'verify("name").canHandle("func").withArgs("arg1", "arg2").andReturns("value").on(sut));';
-    var assumption = {
-      name: 'name',
-      func: 'func',
-      args: stringify(['arg1', 'arg2']),
-      action: 'r:"value"'
-    };
-    expect(createVerification(assumption)).to.eql(expected);
+  describe('create verify string for returnValue assumption', function () {
+    it('simple assumption', function () {
+      var expected = 'verify("name").canHandle("func").withArgs("arg1", "arg2").andReturns("value").on(sut));';
+      var assumption = {
+        name: 'name',
+        func: 'func',
+        args: stringify(['arg1', 'arg2']),
+        action: 'r:\"value\"'
+      };
+      expect(createVerification(assumption)).to.eql(expected);
+    });
+    it('complex assumption', function () {
+      var expected = 'verify("name").canHandle("func").withArgs(5, {"key":"value"}).andReturns({"foo":"bar"}).on(sut));';
+      var assumption = {
+        name: 'name',
+        func: 'func',
+        args: stringify([5, {key: 'value'}]),
+        action: 'r:' + stringify({foo: 'bar'})
+      };
+      expect(createVerification(assumption)).to.eql(expected);
+    });
   });
 
   it('create verify string for callback assumption', function () {
-    var expected = 'verify("name").canHandle("func").withArgs("arg1", "=>function").andCallsCallbackWith(1, "value").on(sut, function (result) {}));';
+    var expected = 'verify("name").canHandle("func").withArgs("arg1", callback).andCallsCallbackWith("value").on(sut, function () {}));';
     var assumption = {
       name: 'name',
       func: 'func',
