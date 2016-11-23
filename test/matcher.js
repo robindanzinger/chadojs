5fbba35b17957a9f0beec72feed9c06549279e53
robindanzinger <robindanzinger@gmx.de>
2016-11-23 17:53:34 +0100

    new lib 'matcher'

--- test/matcher.js
+++ test/matcher.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var expect = require('must');
-//var any = require('../lib/types').any;
-//var anyValue = require('../lib/types').anyValue;
+var any = require('../lib/types').any;
+var anyValue = require('../lib/types').anyValue;
 var lib = require('../lib/matcher');
 var findMatcher = lib.findMatcher;
 var create = lib.createMatcher;
 
 describe('library matcher', function () {
 
-  describe('findMatcher', function () {
+  describe('findMatcher with exact simple values', function () {
     it('returns matching matcher for one argument', function () {
        var matcher = create(1);
        expect(findMatcher(matcher, 1)).to.be(matcher);
@@ -26,10 +26,47 @@ describe('library matcher', function () {
     it('returns undefined when second argument doesn\'t match', function () {
       expect(findMatcher(create(1, 'aString'), 1, 'anotherString')).to.be(undefined);
     });
+    it('returns undefined when args has less arguments than expected', function () {
+      expect(findMatcher(create(1, 2), 1)).to.be(undefined);
+    });
     it('given two matchers, returns the matching matcher', function () {
       var matchers = [create(1), create(2)];
       expect(findMatcher(matchers, 1)).to.be(matchers[0]);
       expect(findMatcher(matchers, 2)).to.be(matchers[1]);
+    });
+  });
+  describe('findMatcher with anyValues', function () {
+    it('returns matching matcher for any value', function () {
+      var matcher = create(anyValue);
+      expect(findMatcher(matcher, 1)).to.be(matcher);
+      expect(findMatcher(matcher, 'aString')).to.be(matcher);
+    });
+    it('returns undefined when called with more args than matcher expects', function () {
+      expect(findMatcher(create(anyValue), 1, 2)).to.be(undefined);
+    });
+    it('returns matcher when called with less args than expected, but expected args are anyValue', function () {
+      var matcher = create(anyValue, anyValue);
+      expect(findMatcher(matcher, 1)).to.be(matcher);
+    });
+  });
+  describe('findMatcher with any', function () {
+    it('returns matching matcher for any', function () {
+      var matcher = create(any);
+      expect(findMatcher(matcher, 1)).to.be(matcher);
+      expect(findMatcher(matcher, 'aString')).to.be(matcher);
+    });
+    it('returns matcher when called with more args than matcher expects', function () {
+      var matcher = create(any);
+      expect(findMatcher(matcher, 1, 2)).to.be(matcher);
+    });
+  });
+  describe('findMatcher with different types of matchers', function () {
+    it('returns the most specific matcher', function () {
+      var matchers = [create(any), create(anyValue, anyValue), create('aString', 1)];
+      expect(findMatcher(matchers, 'aString', 1)).to.be(matchers[2]);
+      expect(findMatcher(matchers, 'aString')).to.be(matchers[1]);
+      expect(findMatcher(matchers, 'aString', 1, 2)).to.be(matchers[0]);
+      expect(findMatcher(matchers)).to.be(matchers[1]);
     });
   });
 });
